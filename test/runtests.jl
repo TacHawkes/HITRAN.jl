7d5bc4a0dfe71656c5f5fe4a7c6112ad5fb00542
Oliver Kliebisch <oliver.kliebisch@dlr.de>
2021-02-16 19:49:03 +0100

    Updates to tests and readme

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,20 @@
 using HITRAN
 using Test
 
-@testset "HITRAN.jl" begin
-    @test_nowarn test_db = open_database("test.sqlite")
+@testset "HITRAN.jl" begin    
     @testset "Total internal partition sums" begin
         for i in HITRAN.isotopologues[:, :global_id]
             q_t0 = HITRAN.isotopologues[HITRAN.isotopologues.global_id .== i, :q_t0][1]            
             tolerance = 10.0^(ceil(Int, log10(q_t0))-3)
             @test isapprox(tips(i, HITRAN.c_T_ref), q_t0, atol=tolerance)
         end
-    end    
+    end
+    @testset "Database and absorption calculation" begin
+        @test_nowarn fetch!("StdAtm", iso_id(7, 1), 13000, 13150, [:standard])
+        @test_nowarn α(["StdAtm"])
+        @test isapprox(absorption_spectrum([0.], 1)[1], 0.0)
+        @test isapprox(transmittance_spectrum([0.], 1)[1], 1.0)
+        @test isapprox(optical_depth([0.], 1)[1], 0.0)
+    end
 end
 
