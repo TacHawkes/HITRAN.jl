6af8e63259af858d063beaf3c4d47e20181d4b48
Oliver Kliebisch <oliver.kliebisch@dlr.de>
2021-02-18 06:47:00 +0100

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,12 @@ using Test
     end
     @testset "Database and absorption calculation" begin
         @test_nowarn fetch!("StdAtm", iso_id(7, 1), 13000, 13150, [:standard])
+        @test isequal(iso_id(["H2O"])[1], 1)
+        @test isequal(iso_id(7, 1), 36)
         @test_nowarn α(["StdAtm"])
         @test_nowarn α(["StdAtm"], :voigt)
         @test_nowarn α(["StdAtm"], :lorentz)
+        @test_nowarn α(["StdAtm"], :gauss)
         @test isapprox(absorption_spectrum([0.], 1)[1], 0.0)
         @test isapprox(transmittance_spectrum([0.], 1)[1], 1.0)
         @test isapprox(optical_depth([0.], 1)[1], 0.0)
